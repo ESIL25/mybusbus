a77ce98d6a57e3d5d77e04ac7c23dda2b6d0e64f
ALPHAKNOW <166359862+ESIL25@users.noreply.github.com>
2024-04-08 18:54:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def get_bus_stops():
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = """
         [out:json];
-        node["highway"="bus_stop"](around:2000, 37.404988,127.106007);
+        node["highway"="bus_stop"](around:500, 37.404988,127.106007);
         out;
         """
     response = requests.get(overpass_url, params={'data': overpass_query})
@@ -20,7 +20,7 @@ def main():
     # 버스 정류장 정보를 가져와 지도 위에 표시
     bus_stops_data = get_bus_stops()
     map_center = [37.403051, 127.107626]
-    m = folium.Map(location=map_center, zoom_start=15)
+    m = folium.Map(location=map_center, zoom_start=20)
 
     for stop in bus_stops_data['elements']:
         lat = stop['lat']

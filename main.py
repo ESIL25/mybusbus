cc8f06ca6a085c6b2e8d875ecfe24cc498b6e0ff
ALPHAKNOW <166359862+ESIL25@users.noreply.github.com>
2024-04-08 19:12:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,19 @@ def get_bus_stops():
     data = response.json()
     return data
 
+def get_bus_locations(api_key, bus_route_id):
+    # 서울시 버스 실시간 위치 정보를 가져오는 함수
+    url = "http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt"
+    params = {
+        "ServiceKey": api_key,
+        "busRouteId": bus_route_id,
+        "startOrd": 1,
+        "endOrd": 13
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
+
 def main():
     st.title('버스 이동 정보 지도')
 
@@ -27,6 +40,15 @@ def main():
         lat = stop['lat']
         lon = stop['lon']
         folium.Marker([lat, lon], popup='Bus Stop').add_to(m)
+
+    # 실시간으로 버스 위치를 가져와 지도에 표시
+    api_key = "PVlQlhVqCM51twmt0Adp4f3LjZLgbpOyYhUbDqt%2FLGW0xf0%2FvjPkfRAN8k6BWndKMws45AtjZBMuFbOn37HRxg%3D%3D"
+    bus_route_id = "100100118"  # 관심 있는 버스 노선 ID로 변경해야 합니다
+    bus_locations_data = get_bus_locations(api_key, bus_route_id)
+    for bus_location in bus_locations_data['ServiceResult']['msgBody']['itemList']:
+        lat = float(bus_location['gpsY'])
+        lon = float(bus_location['gpsX'])
+        folium.Marker([lat, lon], popup='Bus').add_to(m)
 
     folium_static(m)
 
